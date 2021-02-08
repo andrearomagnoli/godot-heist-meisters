bfe16ba0ac69ec1da26cb4d79a775b0809f20d04
Andrea Romagnoli <romagnoli.music@gmail.com>
2021-02-08 21:35:55 +0100

    Add doors

---
+++ Doors/Door.gd
@@ -0,0 +1,27 @@
+extends Area2D
+
+const PLAYER = 1
+const NPC = 4
+
+var can_click = false
+
+
+func _on_Door_body_entered(body):
+	if body.collision_layer == PLAYER:
+		can_click = true
+	elif body.collision_layer == NPC:
+		open()
+
+
+func _on_Door_body_exited(body):
+	if body.collision_layer == PLAYER:
+		can_click = false
+
+
+func _on_Door_input_event(viewport, event, shape_idx):
+	if Input.is_mouse_button_pressed(BUTTON_LEFT) and can_click:
+		open()
+
+
+func open():
+	$AnimationPlayer.play("Open")
