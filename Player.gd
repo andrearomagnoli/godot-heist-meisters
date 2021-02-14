33069cd1e6bd4178589706f4e4bd4c63957daf4b
Andrea Romagnoli <romagnoli.music@gmail.com>
2021-02-14 16:51:39 +0100

    Add box disguise

--- Player.gd
+++ Player.gd
@@ -1,15 +1,31 @@
 extends "res://Characters/TemplateCharacter.gd"
 
 var motion = Vector2()
+var disguised = false
 #var torch_on = true
+
+onready var sprite = $Sprite
+onready var collider = $CollisionShape2D
+onready var occluder = $LightOccluder2D
+
+onready var player_texture = load("res://GFX/PNG/Hitman 1/hitman1_stand.png")
+onready var box_texture = load("res://GFX/PNG/Tiles/tile_130.png")
+onready var human_occluder = load("res://Characters/HumanOccluder.tres")
+onready var human_collider = load("res://Characters/HumanCollider.tres")
+onready var box_occluder = load("res://Characters/BoxOccluder.tres")
+onready var box_collider = load("res://Characters/BoxCollider.tres")
+
 
 func _physics_process(delta):
 	update_movement()
 	motion = move_and_slide(motion)
 
+
 func _input(event):
 	vision_mode_toggle()
+	toggle_disguise()
 #	toggle_torch()
+
 
 func update_movement():
 	
@@ -29,9 +45,35 @@ func update_movement():
 	else:
 		motion.x = lerp(motion.x, 0.0, FRICTION)
 
+
 func vision_mode_toggle():
 	if Input.is_action_just_pressed("toggle_vision_mode"):
 		get_tree().call_group("Interface", "cycle_vision_mode")
+
+
+func toggle_disguise():
+	if Input.is_action_just_pressed("toggle_disguise"):
+		if disguised:
+			reveal()
+		else:
+			disguise()
+
+
+func reveal():
+	sprite.texture = player_texture
+	collider.shape = human_collider
+	occluder.occluder = human_occluder
+	disguised = false
+	collision_layer = 1
+
+
+func disguise():
+	sprite.texture = box_texture
+	collider.shape = box_collider
+	occluder.occluder = box_occluder
+	disguised = true
+	collision_layer = 16
+
 
 func toggle_torch():
 	if Input.is_action_just_pressed("toggle_torch"):
