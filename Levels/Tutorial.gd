f14b09b1fb466bf30d4f781dc19501a0a11beca9
Andrea Romagnoli <romagnoli.music@gmail.com>
2021-02-17 20:45:08 +0100

    Add tutorial boxes animations

--- Levels/Tutorial.gd
+++ Levels/Tutorial.gd
@@ -16,6 +16,7 @@ func update_pointer_position(objective_number):
 	$Tween.start()
 	
 	$MessageSound.play()
+	$TutorialGUI/AnimationPlayer.play('MessageChange')
 	$TutorialGUI/Control/NinePatchRect/Label.text = message.message
 
 
@@ -28,6 +29,7 @@ func _on_DoorObjective_body_entered(body):
 
 
 func _on_NightVisionObjective_body_entered(body):
+	get_tree().call_group('Interface', 'DARK_mode')
 	update_pointer_position(3)
 
 
